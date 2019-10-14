91f02ba84a7ebd085d81c97fd04ef67cde6b10d7
Rizki Ardian <radeite8@gmail.com>
2019-10-14 15:59:41 +0700

    fix(model): use gorm

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/gorilla/mux v1.7.3
-	github.com/mattn/go-sqlite3 v1.11.0
+	github.com/jinzhu/gorm v1.9.11
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/satori/go.uuid v1.2.0
 )
--- todo.go
+++ todo.go
@@ -1,43 +1,34 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	uuid "github.com/satori/go.uuid"
 )
 
 type Todo struct {
-	ID        uuid.UUID `json:"id"`
+	ID        uuid.UUID `json:"id" gorm:"primary_key"`
 	Content   string    `json:"content"`
 	Completed bool      `json:"completed"`
 }
 
 var (
-	db    *sql.DB
-	todos []Todo
+	db        *gorm.DB
+	initDBErr error
+	todos     []Todo
 )
 
 func initDB() {
-	var err error
-	db, err = sql.Open("sqlite3", "./todos.db")
-	if err != nil {
-		log.Fatal(err)
+	db, initDBErr = gorm.Open("sqlite3", "./todos.db")
+	if initDBErr != nil {
+		panic("failed to connect to database")
 	}
-
-	stmt, _ := db.Prepare(
-		`CREATE TABLE IF NOT EXISTS todos (
-			id TEXT NOT NULL UNIQUE,
-			content	TEXT NOT NULL,
-			completed INTEGER NOT NULL,
-			PRIMARY KEY(id)
-		)`,
-	)
-	stmt.Exec()
+	db.AutoMigrate(&Todo{})
 }
 
 func main() {
@@ -58,12 +49,7 @@ func main() {
 func todosAll(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var todos []Todo
-	rows, _ := db.Query("SELECT * FROM todos")
-	for rows.Next() {
-		var todo Todo
-		rows.Scan(&todo.ID, &todo.Content, &todo.Completed)
-		todos = append(todos, todo)
-	}
+	db.Find(&todos)
 	json.NewEncoder(w).Encode(todos)
 }
 
@@ -72,18 +58,8 @@ func todosGet(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	var todo Todo
 	id, _ := uuid.FromString(params["id"])
-	row := db.QueryRow("SELECT * FROM todos WHERE id = ?", id)
-	err := row.Scan(&todo.ID, &todo.Content, &todo.Completed)
-	switch err {
-	case sql.ErrNoRows:
-		http.Error(w, "Not Found", http.StatusNotFound)
-		break
-	case nil:
-		json.NewEncoder(w).Encode(todo)
-		break
-	default:
-		panic(err)
-	}
+	db.Where("id = ?", id).First(&todo)
+	json.NewEncoder(w).Encode(todo)
 }
 
 func todosCreate(w http.ResponseWriter, r *http.Request) {
@@ -92,22 +68,21 @@ func todosCreate(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&todo)
 	id := uuid.NewV4()
 	todo.ID = id
-	stmt, _ := db.Prepare("INSERT INTO todos (id, content, completed) VALUES (?, ?, ?)")
-	stmt.Exec(todo.ID, todo.Content, todo.Completed)
-	stmt.Close()
+	db.Create(&todo)
 	json.NewEncoder(w).Encode(todo)
 }
 
 func todosUpdate(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
+	var body Todo
 	var todo Todo
 	id, _ := uuid.FromString(params["id"])
-	_ = json.NewDecoder(r.Body).Decode(&todo)
-	stmt, _ := db.Prepare("UPDATE todos SET content = ?, completed = ? WHERE id = ?")
-	stmt.Exec(todo.Content, todo.Completed, id)
-	stmt.Close()
-	todo.ID = id
+	_ = json.NewDecoder(r.Body).Decode(&body)
+	db.Where("id = ?", id).First(&todo)
+	todo.Content = body.Content
+	todo.Completed = body.Completed
+	db.Save(&todo)
 	json.NewEncoder(w).Encode(todo)
 }
 
@@ -115,7 +90,7 @@ func todosDelete(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	id, _ := uuid.FromString(params["id"])
-	stmt, _ := db.Prepare("DELETE FROM todos WHERE id = ?")
-	stmt.Exec(id)
-	stmt.Close()
+	var todo Todo
+	db.Where("id = ?", id).Delete(&todo)
+	w.Write([]byte(params["id"] + " deleted"))
 }
